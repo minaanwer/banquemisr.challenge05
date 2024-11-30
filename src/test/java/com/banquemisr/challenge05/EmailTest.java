bb1820616a01a7f6982c69d003027ad78700c5b4
m.lewes@egyptianbanks.net <minaanwer@Minas-MacBook-Pro.local>
2024-11-30 22:30:51 +0200

    add postman collection

--- src/test/java/com/banquemisr/challenge05/EmailTest.java
+++
@@ -1,7 +0,0 @@
-package com.banquemisr.challenge05;
-
-import com.banquemisr.challenge05.util.TaskNotificationService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-
